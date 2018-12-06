d82236568323cb960a10d16a4388234e85d43e68
1055288633@qq.com <978yst4wns>
2018-12-06 22:13:46 +0800

    提交1到100中有多少个含有9的数代码

---
+++ 数9/day3_homework3/数9.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int count = 0;
+	int i = 0;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 10 == 9)
+		{
+			count++;
+		}
+		if (i / 10 == 9)
+		{
+			count++;
+		}
+	}
+	printf("%d\n",count);
+	system("pause");
+	return 0;
+}
